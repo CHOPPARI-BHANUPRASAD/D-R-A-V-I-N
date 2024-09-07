505d30ce57d66f9eaa5a397f3e414062659ed663
CHOPPARI-BHANUPRASAD <nanimudirajchoppari@gmail.com>
2024-09-07 23:36:34 +0530

    another access in mobile

--- script.js
+++ script.js
@@ -59,12 +59,15 @@ const btn = document.querySelector('.talk');
 const content = document.querySelector('.content');
 
 function speak(text) {
-    const text_speak = new SpeechSynthesisUtterance(text);
-    text_speak.rate = 1;
-    text_speak.volume = 1;
-    text_speak.pitch = 1;
-
-    window.speechSynthesis.speak(text_speak);
+    if ('speechSynthesis' in window) {
+        const text_speak = new SpeechSynthesisUtterance(text);
+        text_speak.rate = 1;
+        text_speak.volume = 1;
+        text_speak.pitch = 1;
+        window.speechSynthesis.speak(text_speak);
+    } else {
+        console.error("Speech Synthesis not supported");
+    }
 }
 
 function wishMe() {
@@ -86,6 +89,10 @@ window.addEventListener('load', () => {
 });
 
 const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+if (!SpeechRecognition) {
+    console.error("Speech Recognition not supported");
+}
+
 const recognition = new SpeechRecognition();
 
 recognition.onresult = (event) => {
